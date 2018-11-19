da58c1f32af060cca7d1aabcddfccb22eb2acc01
Tang Yang <ty4081097@gmail.com>
2018-11-18 19:49:49 -0600

    rewrite challenge per instructor feedback

--- challenge.js
+++ challenge.js
@@ -1,33 +1,47 @@
 $(document).ready(readyNow);
+// creating a new variables to hold counts the clicks
+let clicks = 0;
 
-function readyNow(){
-console.log('JQ');
-$('#click').on('click', addDiv);
+function readyNow() {
+    console.log('JQ');
+    $('#button1').on('click', addDiv);
+}// end of readyNow
+
+function addDiv() {
+    clicks++;
+    console.log('this has been clicked');
+    // calling the div in HTML
+    let div = $('#div1');
+    // create new div in to append to DOM
+    let newDiv = '<div class ="red">';
+    // display of clicks
+    newDiv += '<p>' + clicks + '</p>'
+    // swap button
+    newDiv += '<button class = "swapButton">Swap</button>'
+    // delete button
+    newDiv += '<button class = "deleteButton">Delete</button>'
+    // add new div
+    newDiv += '</div>'
+    // append new div above onto DOM
+    div.append(newDiv);
+    // calling deleteDiv 
+    deleteDiv();
 }
 
-function addDiv(){
-console.log('this has been clicked');
-const div = `${'<div></div>'}`
-$('#div1').append(div);
-console.log('div', div);
-createPTag();
-}
+function deleteDiv() {
+    $('.deleteButton').on('click', function () {
+        // "this" is the button element and it's removing it when button is click
+        $(this).parent().remove();
+    })// end of remove delete button
+    swapDiv();
+}// end of deleteDiv
 
-function createPTag(){
-    console.log('this is PTag');
-    let PTag =`${'<p></p>'}`
-    $('#pTag').append(PTag);
-    console.log('this is ptag', PTag)
-    createButton();
-}
+function swapDiv() {
+    $('.swapButton').on('click', function () {
+        console.log('this is swap button');
+        // this toggle the class of yellow in CSS files
+        $(this).parent().toggleClass('yellow');
+    })// end of swap div
+}// end of swapDiv 
 
-function createButton(){
-   
-    $('#swap').append();
-    $('#delete').append();
-
-   
-
-
-}
 
